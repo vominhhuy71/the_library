f4409933dba8df0fca5d70d1a4cf13f014d9dd1b
HuyVo <huy.vminh71@gmail.com>
2021-04-15 15:09:31 +0300

    Create Edit View for Customer

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -43,20 +43,40 @@ namespace library.Controllers
         public ActionResult New()
         {
             var membershipTypes = _context.MembershipTypes.ToList();
-            var viewModel = new NewCustomerViewModel
+            var viewModel = new CustomerFormViewModel
             {
                 MembershipTypes = membershipTypes,
             };
-            return View(viewModel);
+            return View("CustomerForm",viewModel);
         }
 
         [HttpPost]
-        public ActionResult Create(NewCustomerViewModel viewModel)
+        public ActionResult Save(Customer customer)
         {
-            _context.Customers.Add(viewModel.Customer);
+            if(customer.Id==0) _context.Customers.Add(customer);
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthday = customer.Birthday;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+            }
             _context.SaveChanges();
 
             return RedirectToAction("Index", "Customers");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null) return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList(),
+            };
+            return View("CustomerForm", viewModel);
         }
     }
 }
--- ViewModels/CustomerFormViewModel.cs
+++ ViewModels/CustomerFormViewModel.cs
@@ -6,7 +6,7 @@ using System.Web;
 
 namespace library.ViewModels
 {
-    public class NewCustomerViewModel
+    public class CustomerFormViewModel
     {
         public IEnumerable<MembershipType> MembershipTypes { get; set; }
         public Customer Customer { get; set; }
